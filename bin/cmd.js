aab3cb59af55712deee59b6aa2bfbf0304bc80f2
中强 邓 <1246747572@qq.com>
2021-01-27 16:42:28 +0800

    解压默认路径修改

--- bin/cmd.js
+++ bin/cmd.js
@@ -22,15 +22,16 @@ function getFiles(filePath){
   return r;
 }
 
+fileName = path.resolve(fileName);
+
 let isDecode = fileName.indexOf('.wxapkg')>0;
 if(isDecode){// 解码
   let file = fs.readFileSync(fileName);
-
-  let destination = process.argv[3]? process.argv[3] : 'wxapkg.unpack';
+  let writePath = fileName.replace('.wxapkg', '/');
   let wxapkg = new Wxapkg(file);
   let files = wxapkg.decode();
   files.forEach((f)=>{
-      let filePath = __dirname + '/./' + destination + f.name;
+      let filePath = path.join(writePath, f.name);
       let dir = path.dirname(filePath);
       mkdirp(dir, function (err) {
           if (err) return cb(err);
@@ -40,7 +41,6 @@ if(isDecode){// 解码
       });
   })
 }else{// 压缩
-  fileName = path.resolve(fileName);
   let wxapkgFileName = fileName + '.wxapkg';
   let list = getFiles(fileName);
   list.forEach((info)=>{
