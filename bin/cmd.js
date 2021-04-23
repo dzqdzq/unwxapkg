7d0d1f4486bf9c3e9df9cef9a8d1f5be4dbee205
中强 邓 <1246747572@qq.com>
2021-04-23 14:40:27 +0800

    支持批量解压

--- bin/cmd.js
+++ bin/cmd.js
@@ -12,42 +12,61 @@ if(!fileName){
   new Error(`参数不正确`);
 }
 
-if(fs.existsSync(fileName)){
-  new Error(`${fileName} 不存在`);
-}
-
-function getFiles(filePath){
-  let r = [];
-  util.fileDisplay(filePath, r);
-  return r;
-}
-
-fileName = path.resolve(fileName);
-
-let isDecode = fileName.indexOf('.wxapkg')>0;
-if(isDecode){// 解码
-  let file = fs.readFileSync(fileName);
-  let writePath = fileName.replace('.wxapkg', '/');
-  let wxapkg = new Wxapkg(file);
-  let files = wxapkg.decode();
-  files.forEach((f)=>{
-      let filePath = path.join(writePath, f.name);
-      let dir = path.dirname(filePath);
-      mkdirp(dir, function (err) {
-          if (err) return cb(err);
+if (fileName=== '*'){
+  let paths = fs.readdirSync('.');
+  console.log(paths);
   
-          console.log(filePath)
-          fs.writeFileSync(filePath, f.chunk, 'binary')
-      });
+  paths.forEach(fileName=>{
+    fileName = path.resolve(fileName);
+    var extname=path.extname(fileName);
+    if(['.wxapkg', '.wx'].includes(extname)){
+      processOne(fileName)
+    }
   })
-}else{// 压缩
-  let wxapkgFileName = fileName + '.wxapkg';
-  let list = getFiles(fileName);
-  list.forEach((info)=>{
-    info.simName = info.file.replace(fileName, '');
-  });
-  let wxapkg = new Wxapkg();
-  wxapkg.encode(list, wxapkgFileName);
-  // console.log(path.relative(fileName, list[0].file));
-  // console.log(list)
+}else{
+  processOne(fileName);
+}
+
+function processOne(fileName){
+  if(fs.existsSync(fileName)){
+    new Error(`${fileName} 不存在`);
+  }
+  
+  function getFiles(filePath){
+    let r = [];
+    util.fileDisplay(filePath, r);
+    return r;
+  }
+  
+  fileName = path.resolve(fileName);
+  var extname=path.extname(fileName);
+  let isDecode = ['.wxapkg', '.wx'].includes(extname);
+  
+  if(isDecode){// 解码
+    let file = fs.readFileSync(fileName);
+    let writePath = fileName.replace(extname, '/');
+    let wxapkg = new Wxapkg(file);
+    let files = wxapkg.decode();
+    files.forEach((f)=>{
+        let filePath = path.join(writePath, f.name);
+        let dir = path.dirname(filePath);
+        mkdirp(dir, function (err) {
+            if (err) return cb(err);
+    
+            console.log(filePath)
+            fs.writeFileSync(filePath, f.chunk, 'binary')
+        });
+    })
+  }else{// 压缩
+    let wxapkgFileName = fileName + '.wxapkg';
+    let list = getFiles(fileName);
+    list.forEach((info)=>{
+      info.simName = info.file.replace(fileName, '');
+    });
+    let wxapkg = new Wxapkg();
+    wxapkg.encode(list, wxapkgFileName);
+    // console.log(path.relative(fileName, list[0].file));
+    // console.log(list)
+  }
+  
 }
